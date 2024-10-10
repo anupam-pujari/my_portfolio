d6fab4850b5f1c7ebed3ac46b67845bacd3810eb
ANUPAM PUJARI <143895244+anupam-pujari@users.noreply.github.com>
2024-10-10 20:47:00 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// Element toggle function
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+// Sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// Sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
+
+// Contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// Add event to all form input fields
+formInputs.forEach((input) => {
+  input.addEventListener("input", function () {
+    // Check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+  });
+});
+
+// Page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// Add event to all nav links
+navigationLinks.forEach((link, index) => {
+  link.addEventListener("click", function () {
+    pages.forEach((page, i) => {
+      if (index === i) {
+        page.classList.add("active");
+        link.classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        page.classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    });
+  });
+});
